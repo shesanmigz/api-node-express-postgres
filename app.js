ad2f17977550689179d35cd98c039c68b0c0b35a
shesanmigz <shirley.olores@gmail.com>
2018-05-06 23:44:44 -0300

    Added Swagger UI

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 // Set up the express app
 const app = express();
+const router = express.Router();
 
 // Log requests to the console.
 app.use(logger('dev'));
@@ -15,9 +19,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Require routes into the app
 require('./server/routes')(app);
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api', router);
+
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
-    message: 'Welcome to the beginning of nothingness.',
+    message: 'Welcome...',
 }));
-
 module.exports = app;
